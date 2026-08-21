2688fa99434d7454a0e825a4e88d4334ad7309f3
yrsluv <yrslvvvv@gmail.com>
2026-08-21 03:38:00 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+data "yandex_compute_image" "ubuntu-22-04" {
+  family = "ubuntu-2204-lts"
+}
+
+resource "yandex_compute_instance" "vm-01" {
+
+  name        = "vm-01"
+  platform_id = "standard-v3"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      size     = 40
+      type     = "network-ssd"
+      image_id = data.yandex_compute_image.ubuntu-22-04.id
+    }
+  }
+
+  network_interface {
+    subnet_id = data.yandex_vpc_subnet.default-ru-central1-d.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "yc-user:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+data "yandex_vpc_network" "default" {
+  name = "default"
+}
+
+data "yandex_vpc_subnet" "default-ru-central1-d" {
+  name = "default-ru-central1-d"
+}
---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-d"
+  folder_id = "b1g7lugqmq7nnietkvdf"
+}
